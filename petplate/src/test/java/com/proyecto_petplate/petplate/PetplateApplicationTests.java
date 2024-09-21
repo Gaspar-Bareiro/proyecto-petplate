c74c40b273a5f795f20a1700948ebdc7c28effc1
Gaspar-Bareiro <gasparbareir01@gmail.com>
2024-09-21 16:54:13 -0300

    add files
    esqueleto de la carpeta del proyecto

---
+++ petplate/src/test/java/com/proyecto_petplate/petplate/PetplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_petplate.petplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
