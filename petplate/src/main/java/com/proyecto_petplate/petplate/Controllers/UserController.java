f0543c5e4c872589c17a79d0216fade104b6b33c
Franco-Sendros <162157892+Franco-Sendros@users.noreply.github.com>
2024-10-03 16:23:14 -0300

    se hizo un par de correcciones en la DB y se implemento el servicio de login en la api

--- petplate/src/main/java/com/proyecto_petplate/petplate/Controllers/UserController.java
+++ petplate/src/main/java/com/proyecto_petplate/petplate/Controllers/UserController.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto_petplate.petplate.DTO.UserRequestRegisterDTO;
 import com.proyecto_petplate.petplate.services.UserService;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
