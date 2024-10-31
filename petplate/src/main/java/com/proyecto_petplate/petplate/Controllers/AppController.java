6239052757f267017bca44341678345ed1ad3b7f
Franco-Sendros <162157892+Franco-Sendros@users.noreply.github.com>
2024-10-31 20:04:24 -0300

    elimine un importe que estaba demas

--- petplate/src/main/java/com/proyecto_petplate/petplate/Controllers/AppController.java
+++ petplate/src/main/java/com/proyecto_petplate/petplate/Controllers/AppController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.proyecto_petplate.petplate.Entities.EnumUnidadMedida;
 import com.proyecto_petplate.petplate.services.ModelAndViewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
